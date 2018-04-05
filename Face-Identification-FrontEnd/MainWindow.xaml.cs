a9bddea81d1aea2654543bd225b92b8e1b415a42
薛行晨 <1099346563@qq.com>
2018-04-05 16:05:29 +0800

    WPF视频截取帧并显示

--- Face-Identification-FrontEnd/MainWindow.xaml.cs
+++ Face-Identification-FrontEnd/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace Face_Identification_FrontEnd
 {
@@ -20,9 +11,41 @@ namespace Face_Identification_FrontEnd
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Image<Bgr, Byte> image1 = new Image<Bgr, byte>("H:\\壁纸\\1.jpg");
+            // CvInvoke.PutText(image1, "Hello, world", new System.Drawing.Point(image1.Width / 2, image1.Height / 2), Emgu.CV.CvEnum.FontFace.HersheyComplex, 3.0, new Bgr(255.0, 255.0, 255.0).MCvScalar);
+            // CvInvoke.PutText(mat, "Hello1, world", new System.Drawing.Point(image1.Width / 2, image1.Height / 2), Emgu.CV.CvEnum.FontFace.HersheyComplex, 3.0, new Bgr(255.0, 255.0, 255.0).MCvScalar);
+            // CvInvoke.WaitKey(100);
+            Back.Source = BitmapSourceConvert.ToBitmapSource(image1);
+
+            ShowVedio();
+
+
+
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ShowVedio()
+        {
+            VideoCapture capture = new VideoCapture("E:\\1\\1.mp4");
+
+            Mat Frame = new Mat();
+
+            while (capture.Grab())
+            {
+                capture.Read(Frame);
+                CvInvoke.Imshow("1", Frame);
+                CvInvoke.WaitKey(35);
+                Back.Source = BitmapSourceConvert.ToBitmapSource(Frame);
+            }
+            capture.Dispose();
         }
     }
 }
